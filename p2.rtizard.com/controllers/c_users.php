86f276791737b93f8177af186da8623fbd440d6e
Richard Tizard <rtizard@mac.com>
2012-10-31 14:48:08 -0400

    Fixing invisible (to me) typo in login function.

--- p2.rtizard.com/controllers/c_users.php
+++ p2.rtizard.com/controllers/c_users.php
@@ -40,52 +40,53 @@ class users_controller extends base_controller {
 }
 
 	public function login() {
- 
-    # Setup view
-        $this->template->content = View::instance('v_users_login');
-        $this->template->title   = "Login";
-        
-    # Render template
-        echo $this->template;
-    
+
+	# Setup view
+		$this->template->content = View::instance('v_users_login');
+		$this->template->title   = "Login";
+		
+	# Render template
+		echo $this->template;
+	
 }
 
+
 	public function p_login() {
-    
-    # Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
-    $_POST = DB::instance(DB_NAME)->sanitize($_POST);
-    
-    # Hash submitted password so we can compare it against one in the db
-    $_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
-    
-    # Search the db for this email and password
-    # Retrieve the token if it's available
-    $q = "SELECT token 
-        FROM users 
-        WHERE email = '".$_POST['email']."' 
-        AND password = '".$_POST['password']."'";
-    
-    $token = DB::instance(DB_NAME)->select_field($q);   
-                
-    # If we didn't get a token back, login failed
-    if(!$token) {
-            
-        # Send them back to the login page
-        Router::redirect("/users/login/");
-        
-    # But if we did, login succeeded! 
-    } else {
-            
-        # Store this token in a cookie
-        setcookie("token", $token, strtotime('+1 year'), '/');
-        
-        # Send them to the main page - or wherever you want them to go
-        Router::redirect("/");
-                    
-    }
- 
-}
 	
+	# Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
+	$_POST = DB::instance(DB_NAME)->sanitize($_POST);
+	
+	# Hash submitted password so we can compare it against one in the db
+	$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+	
+	# Search the db for this email and password
+	# Retrieve the token if it's available
+	$q = "SELECT token 
+		FROM users 
+		WHERE email = '".$_POST['email']."' 
+		AND password = '".$_POST['password']."'";
+	
+	$token = DB::instance(DB_NAME)->select_field($q);	
+				
+	# If we didn't get a token back, login failed
+	if(!$token) {
+			
+		# Send them back to the login page
+		Router::redirect("/users/login/");
+		
+	# But if we did, login succeeded! 
+	} else {
+			
+		# Store this token in a cookie
+		setcookie("token", $token, strtotime('+1 year'), '/');
+		
+		# Send them to the main page - or whever you want them to go
+		Router::redirect("/");
+					
+	}
+
+}
+
 	public function logout() {
     
     # Generate and save a new token for next login
